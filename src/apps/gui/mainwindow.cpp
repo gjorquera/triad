c3ac5cec4fe1e112e25beca569bce2316850243b
Gaston Jorquera [desktop] <gjorquera@gmail.com>
2009-05-30 17:07:51 -0400

    added opengl initializer

--- src/apps/gui/mainwindow.cpp
+++ src/apps/gui/mainwindow.cpp
@@ -1,3 +1,4 @@
+#include <Viewer/Initializer.h>
 #include <Viewer/OrthographicProjection.h>
 #include "mainwindow.h"
 
@@ -10,10 +11,12 @@ namespace App
         // UI setup
         setupUi(this);
 
+        Viewer::Initializer *initializer;
+        initializer = new Viewer::Initializer();
         Viewer::Projection *projection;
         projection = new Viewer::OrthographicProjection(0.0, 1.0);
 
-        _viewerWidget = new Viewer::Widget(projection, this);
+        _viewerWidget = new Viewer::Widget(initializer, projection, this);
         this->setCentralWidget(_viewerWidget);
     }
 
