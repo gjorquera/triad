7d03afa3e24eca5d7d2689e6a221f8068413b1f8
Gaston Jorquera [desktop] <gjorquera@gmail.com>
2009-06-07 10:34:57 -0400

    added triangle information structure

--- Source/Apps/Gui/Kernel.h
+++ Source/Apps/Gui/Kernel.h
@@ -6,10 +6,26 @@ namespace App
 {
 
     /*!
+     * The information hold by every triangle.
+     */
+    struct TriangleInfo
+    {
+        bool selected;
+        bool lepp;
+
+        TriangleInfo()
+        {
+            selected = false;
+            lepp = false;
+        }
+    };
+
+    /*!
      * The Kernel structure defines the custom Euclid type templates.
      */
     struct Kernel : public Euclid::DefaultKernel
     {
+        typedef TriangleInfo TriangleInfo;
     };
 }
 
