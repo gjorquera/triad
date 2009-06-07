53be347b69c48d5caa05662c5d664d62fdd9f99d
Gaston Jorquera [desktop] <gjorquera@gmail.com>
2009-06-06 23:39:17 -0400

    better docs

--- Source/Libs/Viewer/Figure.h
+++ Source/Libs/Viewer/Figure.h
@@ -10,8 +10,8 @@ namespace Viewer
      * The Figure class defines a drawable component.
      *
      * This class defines the \b decorations method that returns a new instance
-     * of a FigureDecoration based on this figure. This class is inside a \b
-     * paintGL method to render a paint version of it's instance.
+     * of a FigureDecorator based on this figure. This class is created inside a
+     * \b paintGL method to render a paint version of it's instance.
      */
     class Figure : public IFigure
     {
