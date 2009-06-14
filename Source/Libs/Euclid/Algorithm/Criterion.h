e4616db07549337a690c385c3c6e884bd65311a6
Gaston Jorquera [desktop] <gjorquera@gmail.com>
2009-06-14 13:30:49 -0400

    better docs

--- Source/Libs/Euclid/Algorithm/Criterion.h
+++ Source/Libs/Euclid/Algorithm/Criterion.h
@@ -7,6 +7,12 @@ namespace Euclid
 
     /*!
      * Algorithm triangle criteria base class.
+     *
+     * This class is used to select triangles for refinement (refining the
+     * triangle only one time) or to refine until the triangle does not meet the
+     * given criterion (refining the triangle as many times as necessary).
+     *
+     * Concrete criterion must implement the \b test method.
      */
     template <class Kernel>
     class Criterion
