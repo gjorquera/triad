25a297fdb41726fc3998333a34acfa0cab874425
Gaston Jorquera [desktop] <gjorquera@gmail.com>
2009-06-16 15:20:27 -0400

    using new library

--- Source/Libs/Euclid/Algorithm/Parallel/RefineThread.h
+++ Source/Libs/Euclid/Algorithm/Parallel/RefineThread.h
@@ -18,8 +18,9 @@ namespace Euclid
         typedef typename Kernel::Vector Vector;
         typedef Vertex<Kernel>          VertexT;
         typedef Edge<Kernel>            EdgeT;
-        typedef Triangle<Kernel> TriangleT;
+        typedef Triangle<Kernel>        TriangleT;
         typedef TriMesh<Kernel>         TriMesh;
+        typedef LeppLibrary<Kernel>     LeppLibrary;
 
         RefineThread(TriMesh* trimesh, QList<TriangleT*> toRefine)
         {
@@ -56,7 +57,7 @@ namespace Euclid
         TriangleT* refineTriangle(TriangleT* triangle)
         {
             std::cout << ".";
-            TriangleT* neighbor = longestEdgeNeighbor(triangle);
+            TriangleT* neighbor = LeppLibrary::longestEdgeNeighbor(triangle);
             if (0 != neighbor) {
                 bool taken = neighbor->mutex().tryLock();
                 if (! taken) return 0;
@@ -65,7 +66,7 @@ namespace Euclid
             TriangleT* refined = 0;
 
             do {
-                if (0 == neighbor || longestEdgeNeighbor(neighbor) == triangle) {
+                if (LeppLibrary::isTerminal(triangle)) {
                     //refineTerminal(triangle);
                     refined = triangle;
                 } else {
@@ -78,87 +79,6 @@ namespace Euclid
             }
 
             return refined;
-        }
-
-        virtual const EdgeT* longestEdge(const TriangleT* triangle)
-        {
-            int index = longestEdgeIndex(triangle);
-            return triangle->edge(index);
-        }
-
-        virtual void refineTerminal(TriangleT* triangle)
-        {
-            TriMesh* trimesh = _trimesh;
-
-            const EdgeT* edge = longestEdge(triangle);
-            Point point((edge->vector() / 2).terminal());
-            VertexT* newVertex = new VertexT(point);
-            trimesh->addVertex(newVertex);
-
-            TriangleT* neighbor = longestEdgeNeighbor(triangle);
-
-            TriangleT* newTriangle1 = cut(triangle, newVertex);
-
-            if (0 != neighbor) {
-                TriangleT* newTriangle2 = cut(neighbor, newVertex);
-
-                triangle->addNeighbor(newTriangle2);
-                newTriangle2->addNeighbor(triangle);
-
-                neighbor->addNeighbor(newTriangle1);
-                newTriangle1->addNeighbor(neighbor);
-            }
-        }
-
-        virtual int longestEdgeIndex(const TriangleT* triangle)
-        {
-            T max = T();
-            int index = -1;
-
-            for (int i=0; i<3; i++)
-            {
-                const EdgeT* e = triangle->edge(i);
-                if (max < e->vector().distance()) {
-                    max = e->vector().distance();
-                    index = i;
-                }
-            }
-
-            assert(-1 != index);
-
-            return index;
-        }
-
-        virtual TriangleT* cut(TriangleT* triangle, VertexT* newVertex)
-        {
-            TriMesh* trimesh = _trimesh;
-            int index = longestEdgeIndex(triangle);
-
-            VertexT* v1 = const_cast<VertexT*>(triangle->vertex(index));
-            VertexT* v2 = newVertex;
-            VertexT* v3 = const_cast<VertexT*>(triangle->vertex((index+2)%3));
-
-            TriangleT* newTriangle = new TriangleT(v1, v2, v3);
-            trimesh->addTriangle(newTriangle);
-
-            triangle->setVertex((index+2)%3, newVertex);
-
-            TriangleT* neighbor = triangle->neighbor((index+1)%3);
-            if (0 != neighbor) {
-                newTriangle->addNeighbor(neighbor);
-                neighbor->addNeighbor(newTriangle);
-            }
-
-            newTriangle->addNeighbor(triangle);
-            triangle->addNeighbor(newTriangle);
-
-            return newTriangle;
-        }
-
-        TriangleT* longestEdgeNeighbor(TriangleT* triangle)
-        {
-            int index = longestEdgeIndex(triangle);
-            return triangle->neighbor(index);
         }
 
     private:
