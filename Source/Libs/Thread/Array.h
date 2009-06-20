cca28faeb914bd5edf65d67cd0e537476b270dd3
Gaston Jorquera [desktop] <gjorquera@gmail.com>
2009-06-20 19:19:51 -0400

    added array

---
+++ Source/Libs/Thread/Array.h
@@ -0,0 +1,52 @@
+#pragma once
+
+namespace Thread
+{
+
+    template <int N, typename T>
+    class Array
+    {
+    public:
+        Array()
+        {
+            assert(0 < N);
+            for (int i=0; i<N; i++) {
+                _elems[i] = T();
+            }
+        }
+
+        Array(const Array<N,T>& array)
+        {
+            for (int i=0; i<N; i++) {
+                _elems[i] = array._elems[i];
+            }
+        }
+
+        Array<N,T>& operator=(const Array<N,T>& array)
+        {
+            if (this == &vector) return *this;
+
+            for (int i=0; i<N; i++) {
+                _elems[i] = array._elems[i];
+            }
+
+            return *this;
+        }
+
+        T& operator[](const int i)
+        {
+            assert(0 <= i && i < N);
+            return _elems[i];
+        }
+
+        const T& operator[](const int i) const
+        {
+            assert(0 <= i && i < N);
+            return _elems[i];
+        }
+
+    private:
+        T _elems[N];
+    };
+}
+
